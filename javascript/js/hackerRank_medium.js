6ee7447fa7a0478e4da596970f293d7db2c9abc6
Ketan Sawant <ketandutt@gmail.com>
2025-09-29 17:32:07 +0530

    Repeated String
    Hi
    code committed for HackerRank Problem solving in JavaScript (Repeated String)
    Thanks
    Ketan

--- javascript/js/hackerRank_medium.js
+++ javascript/js/hackerRank_medium.js
@@ -42,3 +42,51 @@ function encryption(s) {
 }
 
 console.log(encryption(str))
+
+//==========================================================
+// 02: HackerRank Problem solving in JavaScript (Repeated String)
+//==========================================================
+//https://www.hackerrank.com/challenges/repeated-string/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+function repeatedString(s, n) {
+    // Count 'a's in the original string 's'
+    let countAInS = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'a') {
+            countAInS++;
+        }
+    }
+    // Calculate how many times the full string 's' repeats within 'n' characters
+    const fullRepeats = Math.floor(n / s.length);
+    let totalAs = countAInS * fullRepeats;
+
+    // Calculate the remaining characters after the full repetitions
+    const remainingChars = n % s.length;
+
+    // Count 'a's in the prefix of 's' corresponding to the remaining characters
+    for (let i = 0; i < remainingChars; i++) {
+        if (s[i] === 'a') {
+            totalAs++;
+        }
+    }
+
+    return totalAs;
+}
+
+// Test case
+// let s="aba", n=10
+// let s="a", n=1000000000000
+let s="abcac", n=10
+
+console.log(repeatedString(s, n))
+
+
